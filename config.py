d2503056c59794b7a8820c235a3bb0aa40cb969d
Norman Radtke <radtke@infai.org>
2025-05-14 09:25:43 +0200

    Update configuration
    Make use of more environment variables in preparation for docker deployment.

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
 
 FLASK_ADMIN_SWATCH = 'cosmo'
-SECRET_KEY = '123456790'
-MONGO_URI = 'mongodb://172.17.0.2:27017/adapt'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+MONGO_URI = 'mongodb://{}:{}/{}'.format(
+    os.environ.get("DB_HOST"),
+    os.environ.get("MONGODB_PORT"),
+    os.environ.get("DB_HOST")
+)
 BABEL_DEFAULT_LOCALE = 'de'
 BABEL_DEFAULT_TIMEZONE = 'Europe/Berlin'
 USERNAME = os.environ.get("USER")
 PASSWORD = os.environ.get("PASS")
-BUILDS_DIR = os.path.expanduser("/courses/61eacc691f4b290008b1bf46")
+BUILDS_DIR = os.path.expanduser("{}".format(
+    os.environ.get("BUILDS_DIR")))
 GLOSSARY_JSON = "/app/static/glossary.json"
 ADDITIONAL_COMPONENTS = "/app/static/additional_components.json"
 WTF_CSRF_ENABLED = True  # enables CSRF protection globally
