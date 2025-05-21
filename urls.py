d4deb57e6a70826fd178a6fc7709de767962cba6
Norman Radtke <radtke@infai.org>
2025-05-21 10:14:24 +0200

    Handle more variables in config.py

--- urls.py
+++ urls.py
@@ -1,10 +1,10 @@
-authoring_domain = "https://authoring.creativeartefact.org"
+from config import AUTHORING_DOMAIN
 
 def get_asset_url(asset):
     if type(asset) == dict:
-        return authoring_domain + "/api/asset/serve/" + str(asset.get("_id", ""))
+        return AUTHORING_DOMAIN + "/api/asset/serve/" + str(asset.get("_id", ""))
     else:
         return "#"
 
 def get_editor_url(course_id, page_id):
-    return f"{authoring_domain}/#editor/{course_id}/page/{page_id}"
+    return f"{AUTHORING_DOMAIN}/#editor/{course_id}/page/{page_id}"
