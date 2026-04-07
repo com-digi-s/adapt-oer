a2aa32e932b56d07813b722b3ce8e9d1d8870be2
Norman Radtke <radtke@infai.org>
2026-04-07 13:56:02 +0200

    Fix some issues in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 MONGO_URI = 'mongodb://{}:{}/{}'.format(
     os.environ.get("DB_HOST"),
     os.environ.get("MONGODB_PORT"),
-    os.environ.get("DB_HOST")
+    os.environ.get("DB_NAME")
 )
 BABEL_DEFAULT_LOCALE = 'de'
 BABEL_DEFAULT_TIMEZONE = 'Europe/Berlin'
@@ -13,7 +13,7 @@ USERNAME = os.environ.get("USER")
 PASSWORD = os.environ.get("PASS")
 BUILDS_DIR = os.path.expanduser("{}".format(
     os.environ.get("BUILDS_DIR")))
-GLOSSARY_JSON = "/app/static/glossary.json"
-ADDITIONAL_COMPONENTS = "/app/static/additional_components.json"
+GLOSSARY_JSON = "./static/glossary.json"
+ADDITIONAL_COMPONENTS = "./static/additional_components.json"
 WTF_CSRF_ENABLED = True  # enables CSRF protection globally
 AUTHORING_DOMAIN = os.environ.get("AUTHORING_DOMAIN")
